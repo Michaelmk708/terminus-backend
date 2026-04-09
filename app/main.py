612889a8827adb2d017d5d2354b9a032168e1b96
michaelmk708 <michaelkinuthia708@gmail.com>
2026-04-10 01:08:30 +0300

    frontend-link

--- app/main.py
+++ app/main.py
@@ -27,6 +27,7 @@ app.add_middleware(
         "http://localhost:8000",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:8000",
+        '*'
     ],
     allow_credentials=True,
     allow_methods=["*"],
